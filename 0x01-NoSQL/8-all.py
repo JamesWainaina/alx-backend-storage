bf87fcb1f1e79138cdd606ea2697dfcae5e09558
JamesWainaina <jamesgatheru9905@gmail.com>
2024-06-19 11:51:38 +0300

    pycodestyle fix

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env/ python3
+#!/usr/bin/env python3
 
 """listing all documents in a collection"""
 
@@ -6,9 +6,8 @@ from typing import List
 
 
 def list_all(mongo_collection) -> [List]:
-    """function for listing all documnets in a collection"""
+    """function for listing all documents in a collection"""
     documents = []
     for document in mongo_collection.find():
         documents.append(document)
     return documents
-
