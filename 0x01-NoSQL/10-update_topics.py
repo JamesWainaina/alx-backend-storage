4420881483e5bd4f4f3973e76be3f8490a99620b
JamesWainaina <jamesgatheru9905@gmail.com>
2024-06-19 12:23:35 +0300

    updating all topics bas on the name

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""updating a document in a collection"""
+
+
+def update_topics(mongo_collection, name, topics):
+    """function that update a doucument in a collection base on name"""
+    return mongo_collection.update_many(
+        {name: name}, {"$set": {"topics": topics}})
