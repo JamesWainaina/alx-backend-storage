cc26ac71a7709aee643e602a60b96b838fcc7ceb
JamesWainaina <jamesgatheru9905@gmail.com>
2024-06-19 14:26:19 +0300

    pyhton function that returns all students sorted by average score

---
+++ 0x01-NoSQL/101-students.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+def top_students(mongo_collection):
+    """Returns the top 10 students with the highest average score.
+
+    Args:
+        mongo_collection (Collection): pymongo collection.
+
+    Returns:
+        List[dict]: top 10 students with the highest average score.
+    """
+    students = list(mongo_collection.find())
+
+    for student in students:
+        scores = [topic["score"] for topic in student["topics"]]
+        average_score = sum(scores) / len(scores)
+        student["averageScore"] = average_score
+
+    sorted_students = sorted(
+        students,
+        key=lambda x: x["averageScore"],
+        reverse=True,
+    )
+    return sorted_students
